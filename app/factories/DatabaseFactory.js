053cab76eb9d00970e26943b692635533cdbe67e
John Hamm <johndhamm1@gmail.com>
2016-07-26 10:02:35 -0500

    started db factory

---
+++ app/factories/DatabaseFactory.js
@@ -0,0 +1,59 @@
+"use strict";
+
+app.factory("DatabaseFactory", function(FirebaseURL, $q, $http, AuthFactory){
+
+	let getBoards = function() {
+		let boards = [];
+		return $q(function(resolve, reject) {
+			// console.log("user id?", AuthFactory.getUser());
+			// $http.get(`${FirebaseURL}/items.json?orderBy="uid"&equalTo="${AuthFactory.getUser()}"`)
+			$http.get(`${FirebaseURL}/boards.json?orderBy="uid"&equalTo="aaa666bbb5555"`)
+			.success(function(boardsObj) {
+				console.log("boards", boardsObj);
+				let itemCollection = itemsObject;
+				//create array from object and loop thru keys - saving fb key for each item inside the obj as an id property
+				Object.keys(itemCollection).forEach(function(key){
+					itemCollection[key].id=key;
+					items.push(itemCollection[key]);
+				});
+				resolve (items);
+				console.log("items:", items);
+			})
+			.error(function(error) {
+				reject(error);
+			});
+		});
+	};
+
+	// let postNewItem = function(newItem){
+	// 	// items.push(newItem);
+	// 	return $q(function(resolve, reject){
+	// 		$http.post(`${FirebaseURL}/items.json`,
+	// 			JSON.stringify(newItem))
+	// 		.success(function(ObjFromFirebase){
+	// 			resolve(ObjFromFirebase);
+	// 		})
+	// 		.error(function(error){
+	// 			reject(error);
+	// 		});
+	// 	});
+	// };
+
+	// let deleteItem = function(removeId){
+	// 	let itemUrl = FirebaseURL + "/items/" + removeId + ".json";
+	// 	return $q(function(resolve, reject){
+	// 		$http.delete(itemUrl)
+	// 			.success(function(){
+	// 				resolve();
+	// 			});
+	// 	});
+	// }
+
+
+
+	// 	console.log("item.isCompleted", itemStatus);
+	};
+
+	return {getBoards};
+	
+});
