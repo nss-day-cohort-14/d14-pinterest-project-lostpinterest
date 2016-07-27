2a7e14a89ca6ff89316f2ab5306b2ea9b07081b7
John Hamm <johndhamm1@gmail.com>
2016-07-27 12:49:02 -0500

    stuff

---
+++ app/controllers/PinViewCtrl.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// app.controller("ItemViewCtrl", function($scope, $routeParams, ItemStorage) {
+
+// 	$scope.items = [];
+// 	// $scope.selectedItem = {};
+
+// 	ItemStorage.getItemList()
+// 	 .then(function(itemCollection) {
+// 	 	$scope.items = itemCollection;
+
+// 	 	$scope.selectedItem = $scope.items.filter(function(item){
+// 	 		console.log("item", item);
+// 	 		return item.id === $routeParams.itemId;
+// 	 	})[0];
+// 	});
+
+
+
+// });
